efb33c0011f34eafd34dd985810e4ae0c3bbd80d
Mohammed-shihaf <shihaf@Testable.Cloud>
2026-08-15 12:03:07 +0530

    typescript: remove node_modules and dist from git tracking and add .gitignore

--- dist/service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Service = void 0;
-class Service {
-    processMessage(msg) {
-        return `Processed: ${msg}`;
-    }
-}
-exports.Service = Service;
